1df232ee50f17afc74199c77342f3c7b7efcff39
Nischey Gupta <ngupta16@my.bcit.ca>
2025-02-21 12:06:50 -0800

    inital

---
+++ students-react-ts/src/config/index.ts
@@ -0,0 +1,6 @@
+const Config = {
+    API_BASE_URL: `/api/`,
+};
+
+export default Config;
+  
---
+++ students-react-ts/src/pages/StudentListPage.tsx
@@ -0,0 +1,13 @@
+import StudentList from "../components/StudentList";
+
+const StudentListPage = () => {
+    return (
+        <section>
+        <h1>Student List Page</h1>
+        <StudentList />
+        </section>
+    );
+};
+
+export default StudentListPage;
+  
